33d28a21efb8a67facf11d39a8b4018a336d936a
Ankur Jain <ankurjain@philips.com>
2021-01-09 13:03:01 -0800

    Solution to problem 86

---
+++ problems/86/Problem.h
@@ -0,0 +1,19 @@
+// Copyright 2021
+// Author: Ankur Jain
+
+#pragma once
+#include <climits>
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+#include <sstream>
+#include <stack>
+
+using namespace std;
+
+class Problem
+{
+  public:
+    unsigned int minParentheses(string p);
+};
